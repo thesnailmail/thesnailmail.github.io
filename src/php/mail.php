df918df25370581295c8a9444878340219c94d34
thesnailmail <94803547+thesnailmail@users.noreply.github.com>
2022-01-16 07:57:51 +0300

    Add files via upload

--- src/php/mail.php
+++ src/php/mail.php
@@ -13,8 +13,8 @@ $inputText = $_POST['text'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'example@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'password'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'infofsite@inbox.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '9QY7F38Cz63huh8Yfkt6'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
