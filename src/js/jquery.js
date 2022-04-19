2bcf5ae2ac6ab3738147d1a9fca649d92217174b
thesnailmail <94803547+thesnailmail@users.noreply.github.com>
2022-04-20 01:25:08 +0300

    Add files via upload

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -1,22 +1,2 @@
-
-$(window).scroll(() => {
-
-            let scrollDistance = $(window).scrollTop();
-
-            $(".section").each((i, el) => {
-
-                    if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance) {
-
-                        $("nav a").each((i, el) => {
-
-                                if ($(el).hasClass("active")) {
-
-                                    $(el).removeClass("active");
-                                }
-                            }); $('nav li:eq('+ i +')').find('a').addClass('active');
-                        }
-                    });
-            });
-
 $(".em").mask("+7(999) 999-9999");
 
